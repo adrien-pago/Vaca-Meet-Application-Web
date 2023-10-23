c9208354851ba935ca33509af4c7a2c3a644ca8e
DevTest <115026138+adrien-pago@users.noreply.github.com>
2023-10-23 16:15:55 +0200

    Update Gestion_Camping.js

--- JS/Gestion_Camping.js
+++ JS/Gestion_Camping.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Remplacez cette valeur par l'ID_CAMPING récupéré lors de la connexion
-    const id_camping = "VOTRE_ID_CAMPING";
 
+    // Récupération de l'ID du camping depuis le formulaire
+    var id_camping = document.getElementById('campingForm').elements['id'].value; 
+    console.log(id_camping); // Affiche l'ID_CAMPING dans la console pour debug
+
+    // Récupère les informations du camping avec le bon ID
     function fetchData() {
-        fetch(`fetchData.php?id_camping=${id_camping}`)
-        .then(response => response.json())
+        fetch(`/PHP/API_Fetch.php?id_camping=${id_camping}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur réseau ou serveur');
+            }
+            return response.json();
+        })
         .then(data => {
             let tableBody = document.getElementById("tableBody");
             tableBody.innerHTML = "";
+
             data.forEach(row => {
                 let tr = document.createElement("tr");
                 tr.innerHTML = `
@@ -22,11 +31,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 `;
                 tableBody.appendChild(tr);
             });
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données :', error);
         });
     }
 
     fetchData();
 
+    // Ajout d'une nouvelle ligne au tableau pour une entrée
     document.getElementById("addRow").addEventListener("click", function() {
         let tableBody = document.getElementById("tableBody");
         let tr = document.createElement("tr");
@@ -43,6 +56,7 @@ document.addEventListener("DOMContentLoaded", function() {
         tableBody.insertBefore(tr, tableBody.firstChild);
     });
 
+    // Gestion des événements sur les boutons de chaque ligne du tableau
     document.getElementById("tableBody").addEventListener("click", function(e) {
         if (e.target.classList.contains("edit")) {
             // Code pour éditer la ligne
