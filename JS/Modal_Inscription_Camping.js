2f36858fca0b4991c2cbf642f35d7f8337a4777e
DevTest <115026138+adrien-pago@users.noreply.github.com>
2023-10-28 14:08:53 +0200

    Update Modal_Inscription_Camping.js

--- JS/Modal_Inscription_Camping.js
+++ JS/Modal_Inscription_Camping.js
@@ -1,9 +1,9 @@
 function ouvre_modal() {
-    //Ouverture de la fenetre modal
-    var modal = document.getElementById("modal");
-    modal.style.display = "block"
-    // Ferme la fenetre modal lorsque l'utilisateur clique sur la croix en haut à droite
-    modal.querySelector(".close").addEventListener("click", function() {
-    modal.style.display = "none";
-    });
-  }
+  //Ouverture de la fenetre modal
+  var modal = document.getElementById("modal");
+  modal.style.display = "block"
+  // Ferme la fenetre modal lorsque l'utilisateur clique sur la croix en haut à droite
+  modal.querySelector(".close").addEventListener("click", function() {
+  modal.style.display = "none";
+  });
+}
