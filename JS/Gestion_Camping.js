a0d2dd0d7e872f1d8c18dbd15dff6fe133c2ce45
DevTest <115026138+adrien-pago@users.noreply.github.com>
2023-11-01 17:27:24 +0100

    Update Gestion_Camping.js

--- JS/Gestion_Camping.js
+++ JS/Gestion_Camping.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-<<<<<<< HEAD
 
     // Récupération de l'ID du camping depuis le formulaire
     var id_camping = document.getElementById('campingForm').elements['id'].value; 
     console.log(id_camping); // Affiche l'ID_CAMPING dans la console pour debug
+
+    // Récupération de l'ID du camping depuis le formulaire
+    var nom_camping = document.getElementById('campingForm').elements['camping_name'].value; 
+    console.log(nom_camping); // Affiche l'ID_CAMPING dans la console pour debug
+
+    // Vérifiez si id_camping est non vide
+    if (!id_camping) {
+        console.error('ID du camping est vide');
+        return;
+    }
 
     // Récupère les informations du camping avec le bon ID
     function fetchData() {
@@ -18,21 +27,10 @@ document.addEventListener("DOMContentLoaded", function() {
             let tableBody = document.getElementById("tableBody");
             tableBody.innerHTML = "";
 
-=======
-    // Remplacez cette valeur par l'ID_CAMPING récupéré lors de la connexion
-    const id_camping = "VOTRE_ID_CAMPING";
-
-    function fetchData() {
-        fetch(`fetchData.php?id_camping=${id_camping}`)
-        .then(response => response.json())
-        .then(data => {
-            let tableBody = document.getElementById("tableBody");
-            tableBody.innerHTML = "";
->>>>>>> f051e1761a7f25637fcc9dbeb025f303695b222a
             data.forEach(row => {
                 let tr = document.createElement("tr");
                 tr.innerHTML = `
-                    <td class="hidden">${row.ID_STRUCTURE}</td>
+                    <td>${row.ID_STRUCTURE}</td>
                     <td>${row.NOM_CAMPING}</td>
                     <td>${row.LIBELLE_STRUCTURE}</td>
                     <td>${row.NB_STRUCTURE}</td>
@@ -43,50 +41,160 @@ document.addEventListener("DOMContentLoaded", function() {
                 `;
                 tableBody.appendChild(tr);
             });
-<<<<<<< HEAD
         })
         .catch(error => {
             console.error('Erreur lors de la récupération des données :', error);
-=======
->>>>>>> f051e1761a7f25637fcc9dbeb025f303695b222a
         });
     }
 
     fetchData();
 
-<<<<<<< HEAD
     // Ajout d'une nouvelle ligne au tableau pour une entrée
-=======
->>>>>>> f051e1761a7f25637fcc9dbeb025f303695b222a
     document.getElementById("addRow").addEventListener("click", function() {
         let tableBody = document.getElementById("tableBody");
         let tr = document.createElement("tr");
         tr.innerHTML = `
-            <td class="hidden"></td>
-            <td><input type="text" placeholder="Nom"></td>
+            <td><input type="number" placeholder="Nombre" disabled></td> <!-- Champ grisé -->
+            <td><input type="text" value="${nom_camping}" disabled></td> <!-- Champ grisé avec nom du camping -->
             <td><input type="text" placeholder="Infrastructure"></td>
             <td><input type="number" placeholder="Nombre"></td>
             <td>
-                <button class="save">✔️</button>
+                <button class="save-new">✔️</button> <!-- Classe modifiée pour distinguer ce bouton save -->
                 <button class="cancel">❌</button>
             </td>
         `;
         tableBody.insertBefore(tr, tableBody.firstChild);
     });
 
-<<<<<<< HEAD
+    
     // Gestion des événements sur les boutons de chaque ligne du tableau
-=======
->>>>>>> f051e1761a7f25637fcc9dbeb025f303695b222a
     document.getElementById("tableBody").addEventListener("click", function(e) {
+    var row = e.target.closest("tr"); // Ajout de la variable row pour index les ligne
+
         if (e.target.classList.contains("edit")) {
-            // Code pour éditer la ligne
-        } else if (e.target.classList.contains("delete")) {
-            // Code pour supprimer la ligne
-        } else if (e.target.classList.contains("save")) {
-            // Code pour sauvegarder la nouvelle ligne
-        } else if (e.target.classList.contains("cancel")) {
-            // Code pour annuler l'ajout de la nouvelle ligne
+         // Stockez les valeurs originales dans des attributs de données
+        row.cells[1].dataset.originalValue = row.cells[1].textContent;
+        row.cells[2].dataset.originalValue = row.cells[2].textContent;
+        row.cells[3].dataset.originalValue = row.cells[3].textContent;
+
+        // Remplacez le contenu textuel par des champs de saisie
+        row.cells[1].innerHTML = `<input type="text" value="${row.cells[1].dataset.originalValue}" disabled />`;
+        row.cells[2].innerHTML = `<input type="text" value="${row.cells[2].dataset.originalValue}" />`;
+        row.cells[3].innerHTML = `<input type="number" value="${row.cells[3].dataset.originalValue}" />`;
+        
+        //  Ajout d'un bouton de sauvegarde 
+        e.target.textContent = '✔️';
+        e.target.classList.remove('edit');
+        e.target.classList.add('save');
+
+        // Ajout d'un bouton d'annulation 
+        var cancelBtn = document.createElement("button");
+        cancelBtn.textContent = '❌';
+        cancelBtn.classList.add('cancel-edit');
+        row.cells[4].appendChild(cancelBtn);
+
+    } else if (e.target.classList.contains("save")) {
+        // Obtenez les nouvelles valeurs des champs de saisie
+        var new_libelle_structure = row.cells[2].querySelector("input").value;
+        var new_nb_structure = row.cells[3].querySelector("input").value;
+        var id_structure = row.cells[0].textContent;
+        // Ajout de l'id_camping à la requête
+        var id_camping = document.getElementById('campingForm').elements['id'].value;
+
+        // Envoyez une requête AJAX pour mettre à jour les données
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/PHP/API_Update.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function () {
+            if (xhr.status === 200 && xhr.responseText.trim() === 'Updated successfully') {
+                // Mettez à jour le contenu textuel de la ligne avec les nouvelles valeurs
+                row.cells[2].textContent = new_libelle_structure;
+                row.cells[3].textContent = new_nb_structure;
+
+                // Changez l'icône de sauvegarde en icône d'édition
+                e.target.textContent = '🖉';
+                e.target.classList.remove('save');
+                e.target.classList.add('edit');
+
+                // Supprimez le bouton d'annulation
+                var cancelBtn = row.cells[4].querySelector('.cancel-edit');
+                if (cancelBtn) {
+                    cancelBtn.remove();
+                }
+                
+            } else {
+                console.error('Erreur lors de la mise à jour :', xhr.responseText);
+            }
+        };
+        xhr.send(`id_camping=${id_camping}&id_structure=${id_structure}&libelle_structure=${new_libelle_structure}&nb_structure=${new_nb_structure}`);
+
+    } else if (e.target.classList.contains("cancel-edit")) {
+        // Restaurez le contenu textuel original des cellules depuis les attributs de données
+        row.cells[1].textContent = row.cells[1].dataset.originalValue;
+        row.cells[2].textContent = row.cells[2].dataset.originalValue;
+        row.cells[3].textContent = row.cells[3].dataset.originalValue;
+        
+        // Changez l'icône de sauvegarde en icône d'édition
+        var saveBtn = row.cells[4].querySelector('.save');
+        saveBtn.textContent = '🖉';
+        saveBtn.classList.remove('save');
+        saveBtn.classList.add('edit');
+        
+        // Supprimez le bouton d'annulation
+        e.target.remove();
+
+    } else if (e.target.classList.contains("delete")) {
+        // Code pour supprimer la ligne
+        // Obtenez l'ID de la structure de la ligne à supprimer
+        var id_structure = row.cells[0].textContent;
+        var id_camping = document.getElementById('campingForm').elements['id'].value;
+
+        // Confirmez que l'utilisateur souhaite supprimer cette ligne
+        var confirmDelete = confirm('Êtes-vous sûr de vouloir supprimer cette structure ?');
+        if (confirmDelete) {
+            // Envoyez une requête AJAX pour supprimer la ligne
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/PHP/API_Delete.php', true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onload = function () {
+                if (xhr.status === 200 && xhr.responseText.trim() === 'Deleted successfully') {
+                    // Supprimez la ligne du tableau si la suppression a réussi
+                    row.remove();
+                } else {
+                    console.error('Erreur lors de la suppression :', xhr.responseText);
+                }
+            };
+            xhr.send(`id_structure=${id_structure}&id_camping=${id_camping}`);
         }
+           
+    } else if (e.target.classList.contains("save-new")) {
+        // Obtenez les valeurs des champs de saisie
+        var libelle_structure = row.cells[2].querySelector("input").value;
+        var nb_structure = row.cells[3].querySelector("input").value;
+        var id_camping_input = document.getElementById('campingId');  // Récupérez l'élément d'input de l'ID du camping
+        var id_camping = id_camping_input.value;  // Obtenez la valeur de l'ID du camping
+        
+        // Envoyez une requête AJAX pour insérer les données
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/PHP/API_Insert.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function () {
+            if (xhr.status === 200 && xhr.responseText.trim() === 'Inserted successfully') {
+                // Rechargez les données ou mettez à jour la table comme vous le souhaitez
+                fetchData();
+            } else {
+                console.error('Erreur lors de linsertion :', xhr.responseText);
+            }
+        };
+        // Incluez l'ID du camping dans les données à envoyer
+        var data = `libelle_structure=${libelle_structure}&nb_structure=${nb_structure}&id_camping=${id_camping}`;
+        xhr.send(data);
+
+    } else if (e.target.classList.contains("cancel")) {
+        // Supprimez simplement la ligne parente
+        var rowToRemove = e.target.closest("tr");
+        rowToRemove.remove();
+        }
+        
     });
 });
