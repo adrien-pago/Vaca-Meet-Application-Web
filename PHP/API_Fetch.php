a538df0167d84317d8c3b494eb1f1ec5f9f30243
DevTest <115026138+adrien-pago@users.noreply.github.com>
2023-11-01 17:27:29 +0100

    Update API_Fetch.php

--- PHP/API_Fetch.php
+++ PHP/API_Fetch.php
@@ -1,18 +1,31 @@
 <?php
 include 'config.php';
 
-$id_camping = $_GET['id_camping'];
+try {
+    $id_camping = $_GET['id_camping'];
 
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (empty($id_camping)) {
+        throw new Exception('ID du camping manquant');
+    }
 
-$stmt = $conn->prepare("SELECT STRUCTURE.ID_STRUCTURE, CAMPING.NOM_CAMPING, STRUCTURE.LIBELLE_STRUCTURE, STRUCTURE.NB_STRUCTURE 
-                        FROM STRUCTURE 
-                        JOIN CAMPING ON CAMPING.STRUCTURE = STRUCTURE.ID_STRUCTURE 
-                        WHERE STRUCTURE.ID_CAMPING = :id_camping");
-$stmt->bindParam(':id_camping', $id_camping);
-$stmt->execute();
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($results);
+    $stmt = $conn->prepare("
+        SELECT STRUCTURE.ID_STRUCTURE, CAMPING.NOM_CAMPING, STRUCTURE.LIBELLE_STRUCTURE, STRUCTURE.NB_STRUCTURE 
+        FROM STRUCTURE 
+        JOIN CAMPING ON CAMPING.ID_CAMPING = STRUCTURE.ID_CAMPING 
+        WHERE STRUCTURE.ID_CAMPING = :id_camping
+    ");
+    $stmt->bindParam(':id_camping', $id_camping);
+    $stmt->execute();
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($results);
+
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    http_response_code(500);
+    echo json_encode(['error' => $e->getMessage()]);
+}
 ?>
