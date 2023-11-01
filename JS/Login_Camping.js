c4cce63a3304b4d4cb682d629ce86585f5ffa0a7
DevTest <115026138+adrien-pago@users.noreply.github.com>
2023-11-01 17:27:26 +0100

    Update Login_Camping.js

--- JS/Login_Camping.js
+++ JS/Login_Camping.js
@@ -12,38 +12,6 @@ function login() {
   if (CampingName.trim() == "" || PasswordCamping.trim() == "") {
       alert("Veuillez remplir tous les champs.");
       return false;
-<<<<<<< HEAD
-=======
-    }
-  
-    // Envoyer une requête AJAX pour vérifier les informations de connexion
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/PHP/Verif_login_Camping.php', true);
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        
-        var response = JSON.parse(xhr.responseText);
-        if (response.status === 'success') {
-          // Si l'authentification est réussie, rediriger vers une autre page
-          window.location.href = "Gestion_Camping.html";
-        } else {
-          // Sinon, afficher un message d'erreur
-          alert(response.message);
-        }
-      } else if (xhr.status === 500) {
-        alert('Une erreur s\'est produite du côté du serveur.');
-      } else {
-        alert('Une erreur s\'est produite lors de la vérification des informations de connexion.');
-      }
-    };
-    var formData = new FormData();
-	formData.append('CampingName', CampingName);
-	formData.append('PasswordCamping', PasswordCamping);
-    xhr.send(formData);
-  
-    // Empêcher le formulaire d'être soumis
-    return false;
->>>>>>> f051e1761a7f25637fcc9dbeb025f303695b222a
   }
 
   // Envoyer une requête AJAX pour vérifier les informations de connexion
@@ -56,7 +24,7 @@ function login() {
               // Créer un formulaire
               var form = document.createElement('form');
               form.method = 'POST';
-              form.action = 'Gestion_Camping.html';
+              form.action = 'Gestion_Camping.php';
 
               // Ajouter l'ID du camping comme champ caché
               var hiddenField = document.createElement('input');
@@ -64,6 +32,12 @@ function login() {
               hiddenField.name = 'id';
               hiddenField.value = response.id;
               form.appendChild(hiddenField);
+               // Ajouter le nom du camping comme champ caché
+               var hiddenField2 = document.createElement('input');
+               hiddenField2.type = 'hidden';
+               hiddenField2.name = 'CampingName';
+               hiddenField2.value = CampingName;  // Utilisez la variable CampingName que vous avez déjà récupérée
+               form.appendChild(hiddenField2);
 
               // Ajouter le formulaire au corps de la page et le soumettre
               document.body.appendChild(form);
