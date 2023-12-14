9f31e33c3cf927143805e1f6bfca7bac02fdae55
DevTest <115026138+adrien-pago@users.noreply.github.com>
2023-12-14 14:28:20 +0100

    comit

--- JS/Gestion_Camping.js
+++ JS/Gestion_Camping.js
@@ -55,98 +55,94 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     ////////////////////////////// Gestion Structure ///////////////////////////////////////////////////
-    // Récupère les informations du camping avec le bon ID
-    function fetchData() {
-        fetch(`/PHP/API_STRUCTURE/API_Fetch.php?id_camping=${id_camping}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erreur réseau ou serveur');
-            }
-            return response.json();
-        })
-        .then(data => {
-            let tableBody = document.getElementById("tableBody");
-            tableBody.innerHTML = "";
-
-            data.forEach(row => {
-                let tr = document.createElement("tr");
-                tr.innerHTML = `
-                    <td>${row.ID_STRUCTURE}</td>
-                    <td>${row.NOM_CAMPING}</td>
-                    <td>${row.LIBELLE_STRUCTURE}</td>
-                    <td>${row.NB_STRUCTURE}</td>
-                    <td>
-                        <button class="edit">🖉</button>
-                        <button class="delete">🗑️</button>
-                    </td>
-                `;
-                tableBody.appendChild(tr);
-            });
-        })
-        .catch(error => {
-            console.error('Erreur lors de la récupération des données :', error);
-        });
-    }
-
-    fetchData();
-
-    // Ajout d'une nouvelle ligne au tableau pour une entrée
-    document.getElementById("addRow").addEventListener("click", function() {
+    ////////////////////////////// Gestion Structure ///////////////////////////////////////////////////
+// Récupère les informations du camping avec le bon ID
+function fetchData() {
+    fetch(`/PHP/API_STRUCTURE/API_Fetch.php?id_camping=${id_camping}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erreur réseau ou serveur');
+        }
+        return response.json();
+    })
+    .then(data => {
         let tableBody = document.getElementById("tableBody");
-        let tr = document.createElement("tr");
-        tr.innerHTML = `
-            <td><input type="number" placeholder="Nombre" disabled></td> <!-- Champ grisé -->
-            <td><input type="text" value="${nom_camping}" disabled></td> <!-- Champ grisé avec nom du camping -->
-            <td><input type="text" placeholder="Infrastructure"></td>
-            <td><input type="number" placeholder="Nombre"></td>
-            <td>
-                <button class="save-new">✔️</button> <!-- Classe modifiée pour distinguer ce bouton save -->
-                <button class="cancel">❌</button>
-            </td>
-        `;
-        tableBody.insertBefore(tr, tableBody.firstChild);
+        tableBody.innerHTML = "";
+
+        data.forEach(row => {
+            let tr = document.createElement("tr");
+            tr.innerHTML = `
+                <td>${row.ID_STRUCTURE}</td>
+                <td>${row.NOM_CAMPING}</td>
+                <td>${row.LIBELLE_STRUCTURE}</td>
+                <td>${row.NB_STRUCTURE}</td>
+                <td>
+                    <button class="edit">🖉</button>
+                    <button class="delete">🗑️</button>
+                </td>
+            `;
+            tableBody.appendChild(tr);
+        });
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération des données :', error);
     });
+}
 
-    
-    // Gestion des événements sur les boutons de chaque ligne du tableau
-    document.getElementById("tableBody").addEventListener("click", function(e) {
-    var row = e.target.closest("tr"); // Ajout de la variable row pour index les ligne
+fetchData();
 
-        if (e.target.classList.contains("edit")) {
-         // Stockez les valeurs originales dans des attributs de données
+// Ajout d'une nouvelle ligne au tableau pour une entrée
+document.getElementById("addRow").addEventListener("click", function() {
+    let tableBody = document.getElementById("tableBody");
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+        <td><input type="number" placeholder="ID Structure" disabled></td>
+        <td><input type="text" value="${nom_camping}" disabled></td>
+        <td><input type="text" placeholder="Libellé de la structure"></td>
+        <td><input type="number" placeholder="Nombre"></td>
+        <td>
+            <button class="save-new">✔️</button>
+            <button class="cancel">❌</button>
+        </td>
+    `;
+    tableBody.insertBefore(tr, tableBody.firstChild);
+});
+
+// Gestion des événements sur les boutons de chaque ligne du tableau
+document.getElementById("tableBody").addEventListener("click", function(e) {
+    var row = e.target.closest("tr"); // Cible la ligne la plus proche du bouton cliqué
+
+    if (e.target.classList.contains("edit")) {
+        // Stockage des valeurs originales dans des attributs pour une restauration éventuelle
         row.cells[1].dataset.originalValue = row.cells[1].textContent;
         row.cells[2].dataset.originalValue = row.cells[2].textContent;
         row.cells[3].dataset.originalValue = row.cells[3].textContent;
 
-        // Remplacez le contenu textuel par des champs de saisie
+        // Remplacement du texte par des champs de saisie
         row.cells[1].innerHTML = `<input type="text" value="${row.cells[1].dataset.originalValue}" disabled />`;
         row.cells[2].innerHTML = `<input type="text" value="${row.cells[2].dataset.originalValue}" />`;
         row.cells[3].innerHTML = `<input type="number" value="${row.cells[3].dataset.originalValue}" />`;
-        
-        //  Ajout d'un bouton de sauvegarde 
-        e.target.textContent = '✔️';
-        e.target.classList.remove('edit');
-        e.target.classList.add('save');
 
-        // Ajout d'un bouton d'annulation 
+        // Transformation du bouton "edit" en "save" et ajout d'un bouton "cancel"
+        e.target.textContent = '✔️';
+        e.target.classList.replace('edit', 'save');
+
         var cancelBtn = document.createElement("button");
         cancelBtn.textContent = '❌';
         cancelBtn.classList.add('cancel-edit');
         row.cells[4].appendChild(cancelBtn);
 
     } else if (e.target.classList.contains("save")) {
-        // Obtenez les nouvelles valeurs des champs de saisie
+        // Récupération des nouvelles valeurs
         var new_libelle_structure = row.cells[2].querySelector("input").value;
         var new_nb_structure = row.cells[3].querySelector("input").value;
         var id_structure = row.cells[0].textContent;
-        // Ajout de l'id_camping à la requête
-        var id_camping = document.getElementById('campingForm').elements['id'].value;
 
         // Envoyez une requête AJAX pour mettre à jour les données
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '/PHP/API_STRUCTURE/API_Update.php', true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onload = function () {
+        xhr.onload = function() {
             if (xhr.status === 200 && xhr.responseText.trim() === 'Updated successfully') {
                 // Mettez à jour le contenu textuel de la ligne avec les nouvelles valeurs
                 row.cells[2].textContent = new_libelle_structure;
@@ -154,15 +150,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
                 // Changez l'icône de sauvegarde en icône d'édition
                 e.target.textContent = '🖉';
-                e.target.classList.remove('save');
-                e.target.classList.add('edit');
+                e.target.classList.replace('save', 'edit');
 
                 // Supprimez le bouton d'annulation
                 var cancelBtn = row.cells[4].querySelector('.cancel-edit');
                 if (cancelBtn) {
                     cancelBtn.remove();
                 }
-                
             } else {
                 console.error('Erreur lors de la mise à jour :', xhr.responseText);
             }
@@ -174,21 +168,18 @@ document.addEventListener("DOMContentLoaded", function() {
         row.cells[1].textContent = row.cells[1].dataset.originalValue;
         row.cells[2].textContent = row.cells[2].dataset.originalValue;
         row.cells[3].textContent = row.cells[3].dataset.originalValue;
-        
+
         // Changez l'icône de sauvegarde en icône d'édition
         var saveBtn = row.cells[4].querySelector('.save');
         saveBtn.textContent = '🖉';
-        saveBtn.classList.remove('save');
-        saveBtn.classList.add('edit');
-        
+        saveBtn.classList.replace('save', 'edit');
+
         // Supprimez le bouton d'annulation
         e.target.remove();
 
     } else if (e.target.classList.contains("delete")) {
         // Code pour supprimer la ligne
-        // Obtenez l'ID de la structure de la ligne à supprimer
         var id_structure = row.cells[0].textContent;
-        var id_camping = document.getElementById('campingForm').elements['id'].value;
 
         // Confirmez que l'utilisateur souhaite supprimer cette ligne
         var confirmDelete = confirm('Êtes-vous sûr de vouloir supprimer cette structure ?');
@@ -197,7 +188,7 @@ document.addEventListener("DOMContentLoaded", function() {
             var xhr = new XMLHttpRequest();
             xhr.open('POST', '/PHP/API_STRUCTURE/API_Delete.php', true);
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.onload = function () {
+            xhr.onload = function() {
                 if (xhr.status === 200 && xhr.responseText.trim() === 'Deleted successfully') {
                     // Supprimez la ligne du tableau si la suppression a réussi
                     row.remove();
@@ -207,39 +198,32 @@ document.addEventListener("DOMContentLoaded", function() {
             };
             xhr.send(`id_structure=${id_structure}&id_camping=${id_camping}`);
         }
-           
+
     } else if (e.target.classList.contains("save-new")) {
-        // Obtenez les valeurs des champs de saisie
+        // Récupérez les valeurs des champs de saisie
         var libelle_structure = row.cells[2].querySelector("input").value;
         var nb_structure = row.cells[3].querySelector("input").value;
-        var id_camping_input = document.getElementById('campingId');  // Récupérez l'élément d'input de l'ID du camping
-        var id_camping = id_camping_input.value;  // Obtenez la valeur de l'ID du camping
-        
+
         // Envoyez une requête AJAX pour insérer les données
         var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/PHPAPI_STRUCTURE/API_Insert.php', true);
+        xhr.open('POST', '/PHP/API_STRUCTURE/API_Insert.php', true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onload = function () {
+        xhr.onload = function() {
             if (xhr.status === 200 && xhr.responseText.trim() === 'Inserted successfully') {
-                // Rechargez les données ou mettez à jour la table comme vous le souhaitez
-                fetchData();
+                fetchData(); // Rechargez les données pour mettre à jour le tableau
             } else {
                 console.error('Erreur lors de linsertion :', xhr.responseText);
             }
         };
-        // Incluez l'ID du camping dans les données à envoyer
-        var data = `libelle_structure=${libelle_structure}&nb_structure=${nb_structure}&id_camping=${id_camping}`;
-        xhr.send(data);
+        xhr.send(`libelle_structure=${encodeURIComponent(libelle_structure)}&nb_structure=${encodeURIComponent(nb_structure)}&id_camping=${encodeURIComponent(id_camping)}`);
+        row.remove(); // Supprimez la ligne après insertion
 
     } else if (e.target.classList.contains("cancel")) {
         // Supprimez simplement la ligne parente
-        var rowToRemove = e.target.closest("tr");
-        rowToRemove.remove();
-        }
-        
-    });
+        row.remove();
+    }
+});
 
-    
     ///////////////////////////////// Gestion Activité ///////////////////////////////////////
     function fetchActivities() {
         fetch(`/PHP/API_ACTIVITE/API_Fetch.php?id_camping=${id_camping}`)
--- PHP/Verif_AddCompte_Camping.php
+++ PHP/Verif_AddCompte_Camping.php
@@ -9,28 +9,23 @@ set_include_path(get_include_path() . PATH_SEPARATOR . '/var/www/vhosts/pago-fam
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     http_response_code(405);
     $response_array['status'] = 'error';
-    $response_array['message'] = 'Méthode de requête non autorisée.';
     echo json_encode($response_array);
     die();
-    exit();
+    
 }
 // indiquer explicitement que la réponse est de type JSON.
 header('Content-Type: application/json');
 
 // Inclure PHPMailer dans votre script
 require_once __DIR__ . '/../vendor/autoload.php';
-
 // Importer PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 // Initialiser la variable $response_array
 $response_array = array();
-
-// Se connecter à la base de données (Inclure les informations de connexion depuis le fichier de configuration)
 require_once 'config.php';
 
-// Se connecter à la base de données
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 if ($conn->connect_error) {
     die("La connexion à la base de données a échoué : " . $conn->connect_error);
@@ -42,29 +37,26 @@ $NumeroSiret = $_POST['NumSiretC'];
 $password = $_POST['password'];
 
 // Vérifier si l'adresse email est déjà utilisé dans la base de données
-$sql = "SELECT * FROM CAMPING WHERE EMAIL='$email'";
-$result = $conn->query($sql);
-
+$stmt = $conn->prepare("SELECT * FROM CAMPING WHERE EMAIL = ?");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
 if ($result->num_rows > 0) {
-    // Si l'adresse email existe déjà, retourner un message d'erreur
-    $response_array['status'] = 'error';
-    $response_array['message'] = 'Cette adresse e-mail est déja utilisé';
-    echo json_encode($response_array);
+    echo json_encode(['status' => 'error', 'message' => 'Cette adresse e-mail est déjà utilisée']);
+    $stmt->close();
+    $conn->close();
     die();
-    exit();
 }
 
-// Vérifier si L'userName  est déjà utilisé dans la base de données
-$sql = "SELECT * FROM CAMPING WHERE NOM_CAMPING='$NomCamping'";
-$result = $conn->query($sql);
-
+$stmt = $conn->prepare("SELECT * FROM CAMPING WHERE NOM_CAMPING = ?");
+$stmt->bind_param("s", $NomCamping);
+$stmt->execute();
+$result = $stmt->get_result();
 if ($result->num_rows > 0) {
-    // L'UserName existe déjà, retourner un message d'erreur
-    $response_array['status'] = 'error';
-    $response_array['message'] = 'Ce nom de camping est déjà utilisé';
-    echo json_encode($response_array);
+    echo json_encode(['status' => 'error', 'message' => 'Ce nom de camping est déjà utilisé']);
+    $stmt->close();
+    $conn->close();
     die();
-    exit();
 }
 
 // Hasher le mot de passe avec l'algorithme bcrypt
@@ -102,32 +94,18 @@ try {
      $mail->send();
  }  catch (Exception $e) {
     $response_array['status'] = 'error';
-    $response_array['message'] = "Le message n'a pas pu être envoyé. Erreur : {$mail->ErrorInfo}";
-    echo json_encode($response_array);
-    die();
-    exit();
+    die(); 
 }
 
- 
 // Insérer les données dans la base de données
-$sql = "INSERT INTO CAMPING (NOM_CAMPING, EMAIL, NUM_SIRET, MAP, PASSWORD,  TOKEN_CONFIRM) VALUES ('$NomCamping','$email', '$NumeroSiret', '',  '$hashed_password' , '$confirm_token')";
-if ($conn->query($sql) === TRUE) {
-    // Si l'insertion est réussie, retourner un message de succès
-    $response_array['status'] = 'success';
-    $response_array['message'] = 'Votre compte a été enregistré avec succès. Veuillez finir la validation de votre compte en cliquant sur le lien que nous avons envoyé à votre adresse e-mail.';
-    echo json_encode($response_array);
-    die();
-    exit();
+$stmt = $conn->prepare("INSERT INTO CAMPING (NOM_CAMPING, EMAIL, NUM_SIRET, PASSWORD, TOKEN_CONFIRM) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssss", $NomCamping, $email, $NumeroSiret, $hashed_password, $confirm_token);
+if ($stmt->execute()) {
+    echo json_encode(['status' => 'success', 'message' => 'Votre compte a été enregistré avec succès. Veuillez finir la validation de votre compte en cliquant sur le lien que nous avons envoyé à votre adresse e-mail.']);
 } else {
-    $response_array['db_error'] = $conn->error;
-    // Sinon, retourner un message d'erreur
-    $response_array['status'] = 'error';
-    $response_array['message'] = "Une erreur s'est produite lors de la création de votre compte. Veuillez réessayer.";
-    echo json_encode($response_array);
-    die();
-    exit();
+    echo json_encode(['status' => 'error', 'message' => "Une erreur s'est produite lors de la création de votre compte. Veuillez réessayer."]);
 }
 
-// Fermer la connexion à la base de données
+$stmt->close();
 $conn->close();
 ?>
